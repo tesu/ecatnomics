e9ee75e47cca55e2ac83825fd9ec6ee7690c4d45
3edgy6u <3edgy6u@gmail.com>
2016-02-06 20:00:54 -0500

    trying api access

--- ecatnomics/apps/index/views.py
+++ ecatnomics/apps/index/views.py
@@ -6,4 +6,11 @@ from django.views import generic
 import requests
 
 class IndexView(generic.TemplateView):
+
     template_name = 'index/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        r = requests.get('http://api.reimaginebanking.com/accounts?key=813882c1bc1595ded762f6bb22bd9ee0')
+        context['api'] = r.text
+        return context
