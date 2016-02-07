d2395dadb82c12ae232ba75942f78546b6d0ce24
3edgy6u <3edgy6u@gmail.com>
2016-02-07 11:09:20 -0500

    ;_;

--- ecatnomics/apps/index/views.py
+++ ecatnomics/apps/index/views.py
@@ -66,8 +66,8 @@ class MerchantsIndexView(generic.TemplateView):
         temp = r.json()
         context['api'] = []
         for i in range(10):
-        	context['api'].append(temp[random.randint(0,len(temp))])
-            context['api'][i]['id'] = context['api'][i]['_id']
+            context['api'].append(temp[random.randint(0,len(temp))])
+            context['api'][i-1]['id'] = context['api'][i-1]['_id']
         '''
         context['api'] = r.json()
         for merchant in context['api']:
