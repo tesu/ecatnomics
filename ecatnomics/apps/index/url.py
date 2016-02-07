03fa8afd840381a53ff5a6f0b625d483d9b48310
Hyungju Moon <2016hmoon@tjhsst.edu>
2016-02-06 20:42:41 -0500

    merchants v3

--- ecatnomics/apps/index/url.py
+++ ecatnomics/apps/index/url.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name="index"),
     url(r'^cat/(?P<pk>[a-zA-Z0-9]+)/?$', views.CustomerView.as_view(), name="customer"),
-    url(r'^merchants/(?P<pk>[a-zA-Z0-9]+)/?$', views.ShopView.as_view(), name="merchants"),
-    url(r'^merchantsIndex', views.ShopIndexView.as_view(), name="merchantsIndex"),
+    url(r'^merchants/(?P<pk>[a-zA-Z0-9]+)/?$', views.MerchantsView.as_view(), name="merchants"),
+    url(r'^merchantsIndex', views.MerchantsIndexView.as_view(), name="merchantsIndex"),
 ]
